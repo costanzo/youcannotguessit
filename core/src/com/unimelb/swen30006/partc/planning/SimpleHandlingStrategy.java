a27f00f9466ab92de4478f0b5b6bfaa24c5ab5e5
costanzo <sunshuyi1993@hotmail.com>
2016-10-08 00:09:32 +1100

    finish the first version of handling strategy

--- core/src/com/unimelb/swen30006/partc/planning/SimpleHandlingStrategy.java
+++ core/src/com/unimelb/swen30006/partc/planning/SimpleHandlingStrategy.java
@@ -11,34 +11,43 @@ import com.unimelb.swen30006.partc.tong.Navigation;
 public class SimpleHandlingStrategy implements HandlingStrategy {
     public static float TURNING_SPEED = 10f;
     public static float TURNING_MARGIN = 1f;
-    public static float TURNING_RATE   = 3f;
 
     private Car car;
 
     public Action getAction(PerceptionResponse perceptionResponse, CarState state){
+        float turningAngle = adjustPosture(state.angle, state.shift);
+
         if(state.state == CarState.State.STRAIGHT){
             if(perceptionResponse == null){
-                return new Action(true, false, 0);
+                return new Action(true, false, turningAngle);
             } else if(perceptionResponse.objectType == PerceptionResponse.Classification.TrafficLight){
                 if(this.car.getVelocity().len() > (TURNING_SPEED + TURNING_MARGIN)){
-                    return new Action(false, true, 0);
+                    return new Action(false, true, turningAngle);
                 } else if(this.car.getVelocity().len() < (TURNING_SPEED - TURNING_MARGIN)){
-                    return new Action(true, false, 0);
+                    return new Action(true, false, turningAngle);
                 } else {
-                    return new Action(false, false, 0);
+                    return new Action(false, false, turningAngle);
                 }
             } else {
-
+                return new Action(false, true, turningAngle);
             }
 
         } else if(state.state == CarState.State.LEFT){
-
+            return new Action(true, false, turningAngle);
         } else {
-
+            return new Action(true, false, turningAngle);
         }
     }
 
-    public Action adjustPosture(float angle){
-
+    public float adjustPosture(float angle, float posShift){
+        if(angle >= 0 && posShift >= 0){
+            return 0-angle;
+        } else if( angle < 0 && posShift >= 0){
+            return angle/2;
+        } else if(angle >= 0 && posShift < 0){
+            return angle/2;
+        } else{
+            return 0-angle;
+        }
     }
 }
