9a01d3975b956d295df3a538ca7ce2a155e53d97
Tong <wangtong4348@gmail.com>
2016-10-19 13:52:16 +1100

    comments-tong

--- core/src/com/unimelb/swen30006/partc/planning/HandlingStrategy.java
+++ core/src/com/unimelb/swen30006/partc/planning/HandlingStrategy.java
@@ -7,5 +7,11 @@ import com.unimelb.swen30006.partc.ai.interfaces.PerceptionResponse;
  */
 
 public interface HandlingStrategy {
+    /**
+     *
+     * @param perceptionResponse: the car's surrounding objects.
+     * @param state: the current state of the car
+     * @return: an instance of Action which is the next step the car should take
+     */
     public Action getAction(PerceptionResponse perceptionResponse, CarState state);
 }
